922406beeb7c838acb1b29bfdb56c7d72f74f9fd
Hihiz <public@mail.ru>
2025-09-15 02:46:56 +0300

    Добавить тест получения сеансов фильма

---
+++ CinemaScheduleManagementWeb.Test/Integration/Repository/Session/GetSessionByFilmIdTest.cs
@@ -0,0 +1,15 @@
+﻿namespace CinemaScheduleManagementWeb.Test.Integration.Repository.Session
+{
+    public class GetSessionByFilmIdTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task GetSessionByFilmIdAsyncTest()
+        {
+            // Arrange && Act
+            var result = await sessionRepository.GetSessionByFilmIdAsync(2);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
