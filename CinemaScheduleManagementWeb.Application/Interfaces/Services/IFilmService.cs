398d33dbe0b0202a96ab0a46602a40862d9e673a
Hihiz <public@mail.ru>
2025-09-15 02:39:24 +0300

    Добавить сервис фильмов

---
+++ CinemaScheduleManagementWeb.Application/Interfaces/Services/IFilmService.cs
@@ -0,0 +1,30 @@
+﻿using CinemaScheduleManagementWeb.Application.Dto.Intput.Film;
+using CinemaScheduleManagementWeb.Application.Dto.Output.Film;
+
+namespace CinemaScheduleManagementWeb.Application.Interfaces.Services
+{
+    /// <summary>
+    /// Интерфейс сервиса фильмов.
+    /// </summary>
+    public interface IFilmService
+    {
+        /// <summary>
+        /// Метод получает список фильмов.
+        /// </summary>
+        /// <returns>Список фильмов.</returns>
+        Task<IEnumerable<FilmOutput>> GetFilmsAsync();
+
+        /// Метод получает список фильмов с фильтрами.
+        /// </summary>
+        /// <param name="searchText">Текс для поиска.</param>
+        /// <param name="filterFilmInput">Входная модель.</param>
+        /// <returns>Список фильмов.</returns>
+        Task<IEnumerable<FilmFilterOutput>> GetFilmsFilterAsync(string searchText, FilmFilterInput filterFilmInput);
+
+        /// <summary>
+        /// Метод снимает фильм с проката.
+        /// </summary>
+        /// <param name="filmId">Id фильма.</param>
+        Task ExcludeFilmByFilmIdAsync(int filmId);
+    }
+}
---
+++ CinemaScheduleManagementWeb.Application/Services/FilmService.cs
@@ -0,0 +1,106 @@
+﻿using CinemaScheduleManagementWeb.Application.Dto.Intput.Film;
+using CinemaScheduleManagementWeb.Application.Dto.Output.Film;
+using CinemaScheduleManagementWeb.Application.Interfaces.Repositories;
+using CinemaScheduleManagementWeb.Application.Interfaces.Services;
+using Microsoft.Extensions.Logging;
+
+namespace CinemaScheduleManagementWeb.Application.Services
+{
+    /// <summary>
+    /// Класс реализует методы сервиса фильмов.
+    /// </summary>
+    public class FilmService : IFilmService
+    {
+        private readonly ILogger<FilmService> _logger;
+        private readonly IFilmRepository _filmRepository;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="logger">Логгер.</param>
+        /// <param name="filmRepository">Репозиторий фильмов.</param>
+        public FilmService(ILogger<FilmService> logger,
+            IFilmRepository filmRepository)
+        {
+            _logger = logger;
+            _filmRepository = filmRepository;
+        }
+
+        #region Публичные методы.
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<FilmOutput>> GetFilmsAsync()
+        {
+            try
+            {
+                IEnumerable<FilmOutput> result = await _filmRepository.GetFilmsAsync();
+
+                return result;
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+        }
+
+        /// <inheritdoc />
+        public async Task ExcludeFilmByFilmIdAsync(int filmId)
+        {
+            try
+            {
+                if (filmId <= 0)
+                {
+                    throw new InvalidOperationException("Недопустимый Id фильма. " +
+                                                        $"FilmId: {filmId}.");
+                }
+
+                await _filmRepository.ExcludeFilmByFilmIdAsync(filmId);
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+        }
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<FilmFilterOutput>> GetFilmsFilterAsync(string searchText,
+            FilmFilterInput filterFilmInput)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(searchText))
+                {
+                    throw new InvalidOperationException("Недопустимое значение строки поиска. " +
+                                                        $"SearchText: {searchText}.");
+                }
+
+                if (filterFilmInput is null)
+                {
+                    throw new InvalidOperationException("Недопустимое значение строки поиска. " +
+                                                        $"SearchText: {searchText}.");
+                }
+
+                IEnumerable<FilmFilterOutput> result = await _filmRepository.GetFilmsFilterAsync(searchText,
+                    filterFilmInput);
+
+                return result;
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region Приватные методы.
+
+        #endregion
+    }
+}
