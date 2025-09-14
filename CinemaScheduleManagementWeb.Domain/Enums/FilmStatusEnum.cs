23559b620723bc1529689a3e86d50dab2a09a16a
Hihiz <public@mail.ru>
2025-09-15 02:07:05 +0300

    Добавить перечисления

---
+++ CinemaScheduleManagementWeb.Domain/Enums/FilmStatusEnum.cs
@@ -0,0 +1,18 @@
+﻿namespace CinemaScheduleManagementWeb.Domain.Enums
+{
+    /// <summary>
+    /// Перечисления статусов фильма.
+    /// </summary>
+    public enum FilmStatusEnum
+    {
+        /// <summary>
+        /// Статус фильма в прокате.
+        /// </summary>
+        Active = 1,
+
+        /// <summary>
+        /// Статус фильма снятого с проката
+        /// </summary>
+        InActive = 2
+    }
+}
---
+++ CinemaScheduleManagementWeb.Domain/Enums/SessionStatusEnum.cs
@@ -0,0 +1,18 @@
+﻿namespace CinemaScheduleManagementWeb.Domain.Enums
+{
+    /// <summary>
+    /// Перечисления статусов сеанса.
+    /// </summary>
+    public enum SessionStatusEnum
+    {
+        /// <summary>
+        /// Статус активного сеанса.
+        /// </summary>
+        Active = 1,
+
+        /// <summary>
+        /// Статус отмененного сеанса.
+        /// </summary>
+        Canceled = 2
+    }
+}
