3ec9f7be06fc2839c79df40a7616e1f98a10f325
Hihiz <public@mail.ru>
2025-09-15 02:44:36 +0300

    Добавить тест получения спсика фильмов

---
+++ CinemaScheduleManagementWeb.Test/Integration/Repository/Film/GetFilmsTest.cs
@@ -0,0 +1,15 @@
+﻿namespace CinemaScheduleManagementWeb.Test.Integration.Repository.Film
+{
+    public class GetFilmsTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task GetFilmsAsyncTest()
+        {
+            // Arrange && Act
+            var result = await filmRepository.GetFilmsAsync();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
