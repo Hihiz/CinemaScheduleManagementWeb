f78149acbc17c4f061a23035bbefd482fe796487
Hihiz <public@mail.ru>
2025-09-15 02:37:40 +0300

    Добавить репозиторий настроек кинотеатра

---
+++ CinemaScheduleManagementWeb.Infrastructure/Repositories/CinemaSettingRepository.cs
@@ -0,0 +1,47 @@
+﻿using CinemaScheduleManagementWeb.Application.Dto.Output.CinemaSetting;
+using CinemaScheduleManagementWeb.Application.Interfaces.Repositories;
+using CinemaScheduleManagementWeb.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaScheduleManagementWeb.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Класс реалиует методы репозитория настроек кинотеатра.
+    /// </summary>
+    public class CinemaSettingRepository : ICinemaSettingRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="db">Класс контекста.</param>
+        public CinemaSettingRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        #region Публичные методы.
+
+        /// <inheritdoc />
+        public async Task<CinemaSettingOutput> GetCinemaWorkingHoursAsync()
+        {
+            CinemaSettingOutput result = await _db.CinemaSettings
+                .AsNoTracking()
+                .Select(c => new CinemaSettingOutput
+                {
+                    OpenTime = c.OpenTime,
+                    CloseTime = c.CloseTime
+                })
+                .SingleAsync();
+
+            return result;
+        }
+
+        #endregion
+
+        #region Приватные методы.
+
+        #endregion     
+    }
+}
