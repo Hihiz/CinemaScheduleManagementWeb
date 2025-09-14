c007871a618311843b1f0623e08f08c64e3f26d5
Hihiz <public@mail.ru>
2025-09-15 02:46:27 +0300

    Добавить тест получения списка активных сеансов

---
+++ CinemaScheduleManagementWeb.Test/Integration/Repository/Session/GetActiveSessionsTest.cs
@@ -0,0 +1,43 @@
+﻿using CinemaScheduleManagementWeb.Application.Dto.Intput.Session;
+
+namespace CinemaScheduleManagementWeb.Test.Integration.Repository.Session
+{
+    public class GetActiveSessionsTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task GetActiveSessionsAsyncTest()
+        {
+            // Arrange         
+            var filter = new SessionFilterInput
+            {
+                
+            };
+
+            // Act
+            var result = await sessionRepository.GetActiveSessionsAsync(filter);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task GetActiveSessionsAsyncApplyTest()
+        {
+            // Arrange
+            var filter = new SessionFilterInput
+            {
+                HollId = 1,
+                DateStart = new DateTime(2025, 09, 12, 10, 00, 00, DateTimeKind.Utc),
+                DateEnd = new DateTime(2025, 09, 12, 13, 00, 00, DateTimeKind.Utc),
+            };
+
+          
+            // Act
+            var result = await sessionRepository.GetActiveSessionsAsync(filter);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+
+}
