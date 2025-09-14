b94f20462b47a2d1db3c045554fdd8921cea2e80
Hihiz <public@mail.ru>
2025-09-15 02:44:59 +0300

    Добавить тест получения списка залов

---
+++ CinemaScheduleManagementWeb.Test/Integration/Repository/Hall/GetHallsTest.cs
@@ -0,0 +1,15 @@
+﻿namespace CinemaScheduleManagementWeb.Test.Integration.Repository.Hall
+{
+    public class GetHallsTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task GetHallsAsyncTest()
+        {
+            // Arrange && Act
+            var result = await hallRepository.GetHallsAsync();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
