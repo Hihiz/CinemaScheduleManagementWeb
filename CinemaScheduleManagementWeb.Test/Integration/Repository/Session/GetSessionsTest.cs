f008eacbe2def146f8a792694512d57e9254d5ec
Hihiz <public@mail.ru>
2025-09-15 02:47:09 +0300

    Добавить тест получения списка сеансов

---
+++ CinemaScheduleManagementWeb.Test/Integration/Repository/Session/GetSessionsTest.cs
@@ -0,0 +1,15 @@
+﻿namespace CinemaScheduleManagementWeb.Test.Integration.Repository.Session
+{
+    public class GetSessionsTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task GetSessionsAsyncTest()
+        {
+            // Arrange && Act
+            var result = await sessionRepository.GetSessionsAsync();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
