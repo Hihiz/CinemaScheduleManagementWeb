18e139dee5688032465cec76a6108ffe11b752ab
Hihiz <public@mail.ru>
2025-09-15 02:42:32 +0300

    Добавить контроллер фильмов

---
+++ CinemaScheduleManagementWeb.Api/Controllers/FilmController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaScheduleManagementWeb.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController : ControllerBase
+    {
+    }
+}
