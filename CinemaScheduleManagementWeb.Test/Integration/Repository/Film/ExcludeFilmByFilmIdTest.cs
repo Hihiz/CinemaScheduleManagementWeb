840689ee6d90dc8c33134cd3f17d1e9a83ff6eb1
Hihiz <public@mail.ru>
2025-09-15 02:44:30 +0300

    Добавить тест снятия фильма с проката

---
+++ CinemaScheduleManagementWeb.Test/Integration/Repository/Film/ExcludeFilmByFilmIdTest.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemaScheduleManagementWeb.Test.Integration.Repository.Film
+{
+    public class ExcludeFilmByFilmIdTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task ExcludeFilmByFilmIdAsyncTest()
+        {
+            await filmRepository.ExcludeFilmByFilmIdAsync(2);
+        }
+    }
+}
