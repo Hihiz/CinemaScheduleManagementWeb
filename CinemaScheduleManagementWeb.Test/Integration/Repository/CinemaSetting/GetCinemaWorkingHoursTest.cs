f1d51641574d0015a28fee8965c98e102b32c9a8
Hihiz <public@mail.ru>
2025-09-15 02:44:08 +0300

    Добавить тест получения настроек кинотеатра

---
+++ CinemaScheduleManagementWeb.Test/Integration/Repository/CinemaSetting/GetCinemaWorkingHoursTest.cs
@@ -0,0 +1,15 @@
+﻿namespace CinemaScheduleManagementWeb.Test.Integration.Repository.CinemaSetting
+{
+    public class GetCinemaWorkingHoursTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task GetCinemaWorkingHoursAsyncTest()
+        {
+            // Arrange && Act
+            var result = await cinemaSettingRepository.GetCinemaWorkingHoursAsync();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
