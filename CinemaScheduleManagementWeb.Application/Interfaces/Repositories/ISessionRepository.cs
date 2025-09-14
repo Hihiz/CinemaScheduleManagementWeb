72a328c8cdfa7adf775bc163859229dd29941f17
Hihiz <public@mail.ru>
2025-09-15 02:39:04 +0300

    Добавить репозиторий сеансов

---
+++ CinemaScheduleManagementWeb.Application/Interfaces/Repositories/ISessionRepository.cs
@@ -0,0 +1,47 @@
+﻿using CinemaScheduleManagementWeb.Application.Dto.Intput.Session;
+using CinemaScheduleManagementWeb.Application.Dto.Output.Session;
+using CinemaScheduleManagementWeb.Domain.Entities;
+
+namespace CinemaScheduleManagementWeb.Application.Interfaces.Repositories
+{
+    /// <summary>
+    /// Интерфейс репозитория сеансов.
+    /// </summary>
+    public interface ISessionRepository
+    {
+        /// <summary>
+        /// Метод получает список сеансов.
+        /// </summary>
+        /// <returns>Список сеансов.</returns>
+        Task<IEnumerable<SessionOutput>> GetSessionsAsync();
+
+        /// <summary>
+        /// Метод получает сеансы по Id фильма.
+        /// </summary>
+        /// <param name="filmId">Id фильма.</param>
+        /// <returns>Сеансы.</returns>
+        Task<SessionOutput> GetSessionByFilmIdAsync(int filmId);
+
+        /// <summary>
+        /// Метод получает список активных сеансов.
+        /// </summary>
+        /// <param name="sessionFilterInput">Входная модель.</param>
+        /// <returns>Список активных сеансов.</returns>
+        Task<IEnumerable<SessionOutput>> GetActiveSessionsAsync(SessionFilterInput sessionFilterInput);
+
+        /// <summary>
+        /// Метод проверяет занятость зала по времени.
+        /// </summary>
+        /// <param name="hallId">Id зала.</param>
+        /// <param name="sessionStart">Дата начала сеанса.</param>
+        /// <param name="sessionEnd">Дата завершения сеанса.</param>
+        /// <returns>Признак проверки.</returns>
+        Task<bool> IsHallExistsAsync(int hallId, DateTime sessionStart, DateTime sessionEnd);
+
+        /// <summary>
+        /// Метод создает сеанс.
+        /// </summary>
+        /// <param name="sessionEntity">Модель сеанса.</param>
+        Task CreateSessionAsync(SessionEntity sessionEntity);
+    }
+}
---
+++ CinemaScheduleManagementWeb.Infrastructure/Repositories/SessionRepository.cs
@@ -0,0 +1,215 @@
+﻿using CinemaScheduleManagementWeb.Application.Dto.Intput.Session;
+using CinemaScheduleManagementWeb.Application.Dto.Output.Session;
+using CinemaScheduleManagementWeb.Application.Interfaces.Repositories;
+using CinemaScheduleManagementWeb.Domain.Entities;
+using CinemaScheduleManagementWeb.Domain.Enums;
+using CinemaScheduleManagementWeb.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaScheduleManagementWeb.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Класс реализует методы репозитория сеансов.
+    /// </summary>
+    public class SessionRepository : ISessionRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="db">Класс кнтекста.</param>
+        public SessionRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        #region Публичные методы.
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<SessionOutput>> GetSessionsAsync()
+        {
+            List<SessionOutput> result = await _db.Films
+                 .AsNoTracking()
+                 .Select(s => new SessionOutput
+                 {
+                     FilmId = s.Id,
+                     FilmTitle = s.Title,
+                     AgeLimit = s.AgeLimit,
+                     FilmDuration = s.Duration,
+                     SessionDetailsOutput = s.SessionsEntity.Select(sd => new SessionDetailOutput
+                     {
+                         HallId = sd.HallId,
+                         HallTitle = sd.HallEntity.Title,
+                         SessionStart = sd.SessionStart,
+                         SessionEnd = sd.SessionEnd,
+                         Price = sd.Price,
+                         Status = sd.Status
+                     })
+                     .OrderByDescending(sd => sd.SessionStart)
+                     .ToList()
+                 })
+                 .OrderByDescending(r => r.FilmId)
+                 .ToListAsync();
+
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<SessionOutput>> GetActiveSessionsAsync(SessionFilterInput sessionFilterInput)
+        {
+            IQueryable<FilmEntity> query = _db.Films
+                .AsNoTracking()
+                .Where(s => s.SessionsEntity
+                    .Any(s => s.Status == SessionStatusEnum.Active.ToString()));
+
+            ApplyFilterSessions(sessionFilterInput, query);
+
+            List<SessionOutput> result = await query
+                .Select(f => new SessionOutput
+                {
+                    FilmId = f.Id,
+                    FilmTitle = f.Title,
+                    AgeLimit = f.AgeLimit,
+                    FilmDuration = f.Duration,
+                    SessionDetailsOutput = f.SessionsEntity.Select(s => new SessionDetailOutput
+                    {
+                        HallId = s.HallId,
+                        HallTitle = s.HallEntity.Title,
+                        SessionStart = s.SessionStart,
+                        SessionEnd = s.SessionEnd,
+                        Price = s.Price,
+                        Status = s.Status
+                    }).OrderBy(sd => sd.SessionStart)
+                    .ToList()
+                }).OrderBy(f => f.SessionDetailsOutput!.Min(sd => sd.SessionStart))
+                .ToListAsync();
+
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task<SessionOutput> GetSessionByFilmIdAsync(int filmId)
+        {
+            SessionOutput? result = await _db.Films
+                .AsNoTracking()
+                .Where(f => f.Id == filmId)
+                .Select(s => new SessionOutput
+                {
+                    FilmId = s.Id,
+                    FilmTitle = s.Title,
+                    AgeLimit = s.AgeLimit,
+                    FilmDuration = s.Duration,
+                    SessionDetailsOutput = s.SessionsEntity
+                        .Where(s => s.Status == SessionStatusEnum.Active.ToString())
+                        .Select(sd => new SessionDetailOutput
+                        {
+                            HallId = sd.HallId,
+                            HallTitle = sd.HallEntity.Title,
+                            SessionStart = sd.SessionStart,
+                            SessionEnd = sd.SessionEnd,
+                            Price = sd.Price,
+                            Status = sd.Status
+                        })
+                        .OrderByDescending(sd => sd.SessionStart)
+                        .ToList()
+                })
+                .FirstOrDefaultAsync();
+
+            return result!;
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> IsHallExistsAsync(int hallId, DateTime sessionStart, DateTime sessionEnd)
+        {
+            int techBreak = (await _db.Halls
+                .AsNoTracking()
+                .FirstOrDefaultAsync(h => h.Id == hallId))!.TechBreak;
+
+            bool result = await _db.Sessions
+                .Where(s => s.HallId == hallId && s.Status == SessionStatusEnum.Active.ToString())
+                .AnyAsync(s =>
+                    sessionStart < s.SessionEnd.AddMinutes(techBreak)
+                    && sessionEnd > s.SessionStart
+                );
+
+            return result;
+        }
+
+        /// <inheritdoc />
+        public async Task CreateSessionAsync(SessionEntity sessionEntity)
+        {
+            sessionEntity.Status = SessionStatusEnum.Active.ToString();
+
+            await _db.Sessions.AddAsync(sessionEntity);
+            await _db.SaveChangesAsync();           
+        }
+
+        #endregion
+
+        #region Приватные методы.
+
+        /// <summary>
+        /// Метод применяет фильтры к сеансам..
+        /// </summary>
+        /// <param name="sessionFilterInput">Входная модель.</param>
+        /// <param name="query">Запрос.</param>
+        private void ApplyFilterSessions(SessionFilterInput sessionFilterInput, IQueryable<FilmEntity> query)
+        {
+            // Фильтр по дате.
+            if (sessionFilterInput.DateStart.HasValue && sessionFilterInput.DateEnd.HasValue)
+            {
+                query = query.Where(f => f.SessionsEntity.Any(s => s.SessionStart >= sessionFilterInput.DateStart
+                    && s.SessionEnd <= sessionFilterInput.DateEnd));
+            }
+
+            else if (sessionFilterInput.DateStart.HasValue)
+            {
+                query = query.Where(f => f.SessionsEntity
+                    .Any(s => s.SessionStart >= sessionFilterInput.DateStart));
+            }
+
+            // Фильтр по жанру.
+            if (sessionFilterInput.GenreId > 0)
+            {
+                query = query.Where(f => f.FilmGenresEntity
+                    .Any(fg => fg.GenreId == sessionFilterInput.GenreId));
+            }
+
+            // Фильтр по возрастному ограничению.
+            if (sessionFilterInput.AgeLimit > 0)
+            {
+                query = query.Where(f => f.AgeLimit >= sessionFilterInput.AgeLimit);
+            }
+
+            // Фильтр по продолжительность.
+            if (sessionFilterInput.Duration > 0)
+            {
+                query = query.Where(f => f.Duration >= sessionFilterInput.Duration);
+            }
+
+            // Фильтр по минимальной цене.
+            if (sessionFilterInput.MinPrice > 0)
+            {
+                query = query.Where(f => f.SessionsEntity
+                    .Any(s => s.Price >= sessionFilterInput.MinPrice));
+            }
+
+            // Фильтр по максимальной цене.
+            if (sessionFilterInput.MaxPrice > 0)
+            {
+                query = query.Where(f => f.SessionsEntity
+                    .Any(s => s.Price <= sessionFilterInput.MaxPrice));
+            }
+
+            // Фильтр по залу.
+            if (sessionFilterInput.HollId > 0)
+            {
+                query = query.Where(f => f.SessionsEntity
+                    .Any(s => s.HallId == sessionFilterInput.HollId));
+            }
+        }
+
+        #endregion
+    }
+}
