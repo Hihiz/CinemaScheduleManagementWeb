c947df3cf4b75ea7670a57b1f496c3d4bf3b5721
Hihiz <public@mail.ru>
2025-09-15 02:39:36 +0300

    Добавить сервис залов

---
+++ CinemaScheduleManagementWeb.Application/Interfaces/Services/IHallService.cs
@@ -0,0 +1,16 @@
+﻿using CinemaScheduleManagementWeb.Application.Dto.Output.Hall;
+
+namespace CinemaScheduleManagementWeb.Application.Interfaces.Services
+{
+    /// <summary>
+    /// Сервис интерфейса зала.
+    /// </summary>
+    public interface IHallService
+    {
+        /// <summary>
+        /// Метод получает список залов.
+        /// </summary>
+        /// <returns>Спсиок залов.</returns>
+        Task<IEnumerable<HallOutput>> GetHallsAsync();
+    }
+}
---
+++ CinemaScheduleManagementWeb.Application/Services/HallService.cs
@@ -0,0 +1,53 @@
+﻿using CinemaScheduleManagementWeb.Application.Dto.Output.Hall;
+using CinemaScheduleManagementWeb.Application.Interfaces.Repositories;
+using CinemaScheduleManagementWeb.Application.Interfaces.Services;
+using Microsoft.Extensions.Logging;
+
+namespace CinemaScheduleManagementWeb.Application.Services
+{
+    /// <summary>
+    /// Класс реализует методы сервиса залов.
+    /// </summary>
+    public class HallService : IHallService
+    {
+        private readonly ILogger<HallService> _logger;
+        private readonly IHallRepository _hallRepository;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="logger">Логгер.</param>
+        /// <param name="hallRepository">Репозиторий залов.</param>
+        public HallService(ILogger<HallService> logger,
+            IHallRepository hallRepository)
+        {
+            _logger = logger;
+            _hallRepository = hallRepository;
+        }
+
+        #region Публичные методы.
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<HallOutput>> GetHallsAsync()
+        {
+            try
+            {
+                IEnumerable<HallOutput> result = await _hallRepository.GetHallsAsync();
+
+                return result;
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region Приватные методы.
+
+        #endregion       
+    }
+}
