0955d8c5d04d85316ef1f7b62e0622f3cfd207e4
Hihiz <public@mail.ru>
2025-09-15 02:38:19 +0300

    Добавить репозиторий залов

---
+++ CinemaScheduleManagementWeb.Application/Interfaces/Repositories/IHallRepository.cs
@@ -0,0 +1,16 @@
+﻿using CinemaScheduleManagementWeb.Application.Dto.Output.Hall;
+
+namespace CinemaScheduleManagementWeb.Application.Interfaces.Repositories
+{
+    /// <summary>
+    /// Интерфейс репозитория залов.
+    /// </summary>
+    public interface IHallRepository
+    {
+        /// <summary>
+        /// Метод получает список залов.
+        /// </summary>
+        /// <returns>Спсиок залов.</returns>
+        Task<IEnumerable<HallOutput>> GetHallsAsync();
+    }
+}
---
+++ CinemaScheduleManagementWeb.Infrastructure/Repositories/HallRepository.cs
@@ -0,0 +1,50 @@
+﻿using CinemaScheduleManagementWeb.Application.Dto.Output.Hall;
+using CinemaScheduleManagementWeb.Application.Interfaces.Repositories;
+using CinemaScheduleManagementWeb.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaScheduleManagementWeb.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Класс реализует методы репозитория зала.
+    /// </summary>
+    public class HallRepository : IHallRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="db">Класс контекста.</param>
+        public HallRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        #region Публичные методы.
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<HallOutput>> GetHallsAsync()
+        {
+            List<HallOutput> result = await _db.Halls
+             .AsNoTracking()
+             .Select(h => new HallOutput
+             {
+                 Id = h.Id,
+                 Title = h.Title,
+                 TotalSeat = h.TotalSeat,
+                 TechBreak = h.TechBreak
+             })
+             .OrderByDescending(h => h.Id)
+             .ToListAsync();
+
+            return result;
+        }
+
+        #endregion
+
+        #region Приватные методы.
+
+        #endregion       
+    }
+}
