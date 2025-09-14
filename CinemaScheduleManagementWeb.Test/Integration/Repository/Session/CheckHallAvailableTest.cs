387ca7fd0881a0fab42b543e31bd910884f84786
Hihiz <public@mail.ru>
2025-09-15 02:46:11 +0300

    Добавить тест проверки занятости зала

---
+++ CinemaScheduleManagementWeb.Test/Integration/Repository/Session/CheckHallAvailableTest.cs
@@ -0,0 +1,33 @@
+﻿namespace CinemaScheduleManagementWeb.Test.Integration.Repository.Session
+{
+    public class CheckHallAvailableTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task IsHallExistsAsyncTest()
+        {
+            // Arrange
+            var start = new DateTime(2025, 9, 12, 15, 00, 00, DateTimeKind.Utc);
+            var end = new DateTime(2025, 9, 12, 15, 10, 00, DateTimeKind.Utc);
+
+            //  Act
+            var result = await sessionRepository.IsHallExistsAsync(1, start, end);
+            
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task IsHallExistsAsyncExistsTest()
+        {
+            // Arrange
+            var start = new DateTime(2025, 9, 12, 14, 30, 00, DateTimeKind.Utc);
+            var end = new DateTime(2025, 9, 12, 15, 30, 00, DateTimeKind.Utc);
+
+            //  Act
+            var result = await sessionRepository.IsHallExistsAsync(1, start, end);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
